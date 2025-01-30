f220c5d9a37c6b457b8e439643330d8b34400761
aleksmn <al-mn@yandex.ru>
2025-01-30 12:58:09 +0300

    Initial commit

---
+++ cards.c
@@ -0,0 +1,29 @@
+/* Программа для оценивания важности карт.
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char card_name[3];
+	puts("Название карты: ");
+	scanf("%2s", card_name);
+	
+	int val = 0;
+	if (card_name[0] == 'K') {
+		val = 10;
+	} else if (card_name[0] == 'Q') {
+		val = 10;
+	} else if (card_name[0] == 'J') {
+		val = 10;
+	} else if (card_name[0] == 'A') {
+		val = 11;
+	} else {
+		val = atoi(card_name);
+	}
+
+	printf("Ценность карты: %i\n", val);
+
+	return 0;
+}
---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\n");
+    printf("This is my first c programm!\n");
+    return 0;
+}
